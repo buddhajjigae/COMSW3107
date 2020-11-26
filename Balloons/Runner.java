b37d37cae45c6cc977edfe6f56099f6a8de77479
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-11-26 18:03:49 -0500

    Update and rename HW4Runner.java to Runner.java
    Changed class name to Runner and added comments

---
+++ Balloons/Runner.java
@@ -0,0 +1,16 @@
+
+/**
+ * The Runner class will run the program by calling start() in Control. See the 
+ * Control class for details of what start() will do. 
+ * 
+ * @author Alex Yu ajy2116 based on John Kender's alteration of Cay Horstmann's code.
+ * (Horstmann's translate and draw logic used as guide)
+ *
+ */
+
+public class Runner {
+	public static void main(String[] args) {
+		Control control = new Control();
+		control.start();
+	}
+}
