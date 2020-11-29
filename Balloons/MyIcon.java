9d70cb3d0d09a0698d378866a99e1e48d8f5e404
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-11-29 02:30:08 -0500

    Update MyIcon.java
    Changed objects to MovingObject to reflect new logic

--- Balloons/MyIcon.java
+++ Balloons/MyIcon.java
@@ -1,7 +1,7 @@
 
 /**
  * The MyIcon class will create an Icon based on the given width and height and
- * paint the objects in the objects ArrayList in the Icon. 
+ * paint the objects in objects in the Icon. 
  * 
  * @author Alex Yu ajy2116 based on John Kender's alteration of Cay Horstmann's code.
  * (Horstmann's logic used as guide) 
@@ -9,23 +9,22 @@
  */
 
 import java.awt.*;
-import java.util.ArrayList;
 import javax.swing.*;
 
 public class MyIcon implements Icon {
 	private int iconWidth;
 	private int iconHeight;
-	private ArrayList<MovingObject> objects;
-	
+	private MovingObject object;
+
 	/**
-	 * Class constructor used to set the icon width, height, and objects. 
+	 * Class constructor used to set the icon width, height, and object.
 	 * 
-	 * @param objects the objects of the icon
-	 * @param w the width of the icon
-	 * @param h the height of the icon
+	 * @param object the MovingObject of the icon
+	 * @param w      the width of the icon
+	 * @param h      the height of the icon
 	 */
-	public MyIcon(ArrayList<MovingObject> objects, int w, int h) {
-		this.objects = objects;
+	public MyIcon(MovingObject object, int w, int h) {
+		this.object = object;
 		this.iconWidth = w;
 		this.iconHeight = h;
 	}
@@ -49,17 +48,16 @@ public class MyIcon implements Icon {
 	}
 
 	/**
-	 * Method to paint the icon
+	 * Method to paint the MovingObject.
 	 * 
-	 * @param component the component that will be used if the icon has no image observer
-	 * @param graphic the context of the graphic
-	 * @param x the icon's top-left corner x coordinate
-	 * @param y the icon's top-left corner y coordinate
+	 * @param component the component that will be used if the icon has no image
+	 *                  observer
+	 * @param graphic   the context of the graphic
+	 * @param x         the icon's top-left corner x coordinate
+	 * @param y         the icon's top-left corner y coordinate
 	 */
 	public void paintIcon(Component component, Graphics graphic, int x, int y) {
 		Graphics2D graphics2D = (Graphics2D) graphic;
-		for(MovingObject object : objects) {
-			object.draw(graphics2D);
-		}
+		object.draw(graphics2D);
 	}
 }
