0399ffe4eb82c7d89ae6eef44c89b406e657e74f
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-11-29 02:30:38 -0500

    Update Runner.java

--- Balloons/Runner.java
+++ Balloons/Runner.java
@@ -1,10 +1,9 @@
 
 /**
- * The Runner class will run the program by calling start() in Control. See the 
- * Control class for details of what start() will do. 
+ * The Runner class will run the program by calling start() in Control. See the
+ * Control class for details of what start() will do.
  * 
- * @author Alex Yu ajy2116 based on John Kender's alteration of Cay Horstmann's code.
- * (Horstmann's translate and draw logic used as guide)
+ * @author Alex Yu ajy2116
  *
  */
 
