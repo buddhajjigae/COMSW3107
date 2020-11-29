bdd3cdb35c9531be7dace45e80cd073c0040980f
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-11-29 01:54:00 -0500

    Update Balloon.java
    Changed logic of arguments to match new logic structure for how balloons will translate

--- Balloons/Balloon.java
+++ Balloons/Balloon.java
@@ -1,71 +1,93 @@
+
+/**
+ * The Balloon class defines a hot air balloon object as a MovingObject. It contains a translate 
+ * function that changes the x and y position of a balloon. Draw is used to paint the balloon object
+ * on the respective Icon. 
+ * 
+ * @author Alex Yu ajy2116 based on John Kender's alteration of Cay Horstmann's code.
+ * (Horstmann's translate and draw logic used as guide) 
+ *
+ */
+
 import java.awt.*;
 import java.awt.geom.*;
 
-/** * * @author jrk based on cay horstmann * */
-
 public class Balloon implements MovingObject {
-	private int x;
-	private int y;
-	private int unit;
-	private final double cableAngle = Math.PI / 3;
-	private final double angle2 = Math.PI / 8;
-	private final double BASKETDISTANCE = 2.0;
-	private final double BASKETWIDTH = .4;
-	private final double BASKETHEIGHT = .25;
-	GeneralPath path;
+	private double x;
+	private double y;
+	private double size;
+	private GeneralPath path;
+	private final double CABLE_ANGLE = Math.PI / 3;
+	private final double BASKET_ANGLE = Math.PI / 8;
+	private final double BASKET_DISTANCE = 2.0;
+	private final double BASKET_WIDTH = .4;
+	private final double BASKET_HEIGHT = .25;
 
-	public Balloon(int x, int y, int unit) {
+	/**
+	 * Class constructor used to initialize the initial x and y positions of the
+	 * balloon along with the balloon size.
+	 * 
+	 * @param x    the x coordinate of the balloon
+	 * @param y    the y coordinate of the balloon
+	 * @param size the size of the balloon
+	 */
+	public Balloon(double x, double y, double size) {
 		this.x = x;
 		this.y = y;
-		this.unit = unit;
+		this.size = size;
+
 	}
 
-	public void translate(int xChange, int yChange, double randomDrift) {
-		if (xChange == 0) {
-			x += randomDrift;
-			y += yChange;
-			path.moveTo(x, y);
-		} else {
-			x += xChange;
-			y += yChange;
-			path.moveTo(x, y);
-		}
+	/**
+	 * This method is used to change the x and y coordinates of the balloon by using
+	 * the given xChange and yChange arguments to set a new x and y value.
+	 * 
+	 * @param xChange the amount that the balloon will move on the x-axis
+	 * @param yChange the amount that the balloon will move on the y-axis
+	 */
+	public void translate(double xChange, double yChange) {
+		x += xChange;
+		y += yChange;
+		path.moveTo(x, y);
 	}
-	
-	public void draw(Graphics2D g2D) {
-		// envelope
-		int envelopeRadius = unit / 2;
-		double balloonHeight = unit * BASKETHEIGHT + envelopeRadius + envelopeRadius * BASKETDISTANCE;
-		int envelopeXUp = x;
-		int envelopeYUp = y - (int) balloonHeight;
-		//int envelopeYBot = envelopeYUp + unit;
+
+	/**
+	 * Draw defines how the cables, envelope, and basket of the balloon will be
+	 * drawn relative to each other at a given x and y coordinate and size. It will
+	 * then append the three pieces onto a single GeneralPath.
+	 * 
+	 * @param graphics2D the graphics2D that the balloon will be drawn onto
+	 */
+	public void draw(Graphics2D graphics2D) {
+		// Envelope
+		double envelopeRadius = size / 2;
+		double balloonHeight = size * BASKET_HEIGHT + envelopeRadius + envelopeRadius * BASKET_DISTANCE;
+		double envelopeXUp = x;
+		double envelopeYUp = y - balloonHeight;
 		Ellipse2D.Double envelope = new Ellipse2D.Double(envelopeXUp, envelopeYUp, envelopeRadius * 2,
 				envelopeRadius * 2);
-
-		// cables
-		double cableLXTop = envelopeXUp + envelopeRadius + envelopeRadius * Math.cos(Math.PI - cableAngle);
-		double cableLYTop = envelopeYUp + envelopeRadius + envelopeRadius * Math.sin(Math.PI - cableAngle);
-		double cableRXTop = envelopeXUp + envelopeRadius + envelopeRadius * Math.cos(cableAngle);
-		double cableRYTop = envelopeYUp + envelopeRadius + envelopeRadius * Math.sin(cableAngle);
-		double cableXLBot = envelopeXUp + envelopeRadius - BASKETWIDTH * 10;
-		double cableXYBot = envelopeXUp + envelopeRadius + BASKETWIDTH * 10;
-		double cableYBot = envelopeYUp + envelopeRadius + envelopeRadius * BASKETDISTANCE;
+		// Cables
+		double cableLXTop = envelopeXUp + envelopeRadius + envelopeRadius * Math.cos(Math.PI - CABLE_ANGLE);
+		double cableLYTop = envelopeYUp + envelopeRadius + envelopeRadius * Math.sin(Math.PI - CABLE_ANGLE);
+		double cableRXTop = envelopeXUp + envelopeRadius + envelopeRadius * Math.cos(CABLE_ANGLE);
+		double cableRYTop = envelopeYUp + envelopeRadius + envelopeRadius * Math.sin(CABLE_ANGLE);
+		double cableXLBot = envelopeXUp + envelopeRadius - BASKET_WIDTH * size / 6.5;
+		double cableXYBot = envelopeXUp + envelopeRadius + BASKET_WIDTH * size / 6.5;
+		double cableYBot = envelopeYUp + envelopeRadius + envelopeRadius * BASKET_DISTANCE;
 		Line2D.Double cableL = new Line2D.Double(cableLXTop, cableLYTop, cableXLBot, cableYBot);
 		Line2D.Double cableR = new Line2D.Double(cableRXTop, cableRYTop, cableXYBot, cableYBot);
-		
-		// basket
-		double basketXTop = cableLXTop + envelopeRadius + envelopeRadius * Math.cos(Math.PI - angle2);
+		// Basket
+		double basketXTop = cableLXTop + envelopeRadius + envelopeRadius * Math.cos(Math.PI - BASKET_ANGLE);
 		double basketYTop = cableYBot;
-		Rectangle2D.Double basket = new Rectangle2D.Double(basketXTop, basketYTop, unit * BASKETWIDTH,
-				unit * BASKETHEIGHT);
-		
+		Rectangle2D.Double basket = new Rectangle2D.Double(basketXTop, basketYTop, size * BASKET_WIDTH,
+				size * BASKET_HEIGHT);
 		path = new GeneralPath();
 		path.append(envelope, false);
 		path.append(basket, false);
 		path.append(cableL, false);
 		path.append(cableR, false);
 
-		g2D.fill(path);
-		g2D.draw(path);
+		graphics2D.fill(path);
+		graphics2D.draw(path);
 	}
 }
