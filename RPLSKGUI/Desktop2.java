a579dfb4eab8b4e83423835d1adaae42198b67fd
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-12-10 03:52:42 -0500

    Update Desktop2.java
    Changed logic

--- RPLSKGUI/Desktop2.java
+++ RPLSKGUI/Desktop2.java
@@ -18,7 +18,7 @@ import javax.swing.*;
 
 public class Desktop2 {
 	
-	ArrayList<Throwable> throwList = new ArrayList<Throwable>();
+	static ArrayList<Throwable> throwList = new ArrayList<Throwable>();
 	
 	
 	// Java 8 independent thread idiom, from tutorial
@@ -36,11 +36,15 @@ public class Desktop2 {
 		System.out.println("Created GUI on EDT? " + SwingUtilities.isEventDispatchThread());
 		// usual frame idioms
 		JFrame myFrame = new JFrame("Desktop version");
-		DesktopPanel myPanel = new DesktopPanel();
-		//myFrame.add(myPanel);
-		StringMovingObject obj = new StringMovingObject.Builder().throwName("Scissors").throwX(500).throwY(0).build();
 		StringMovingObject obj2 = new StringMovingObject("Rock", 500, 0);
-		myFrame.add(obj2);
+		StringMovingObject obj = new StringMovingObject.Builder().throwName("Scissors").throwX(500).throwY(0).build();
+		//DesktopPanel myPanel = new DesktopPanel(obj2);
+		generateThrowables();
+		DesktopPanel myPanel = new DesktopPanel(throwList);
+		//myFrame.add(myPanel);
+
+
+		myFrame.add(myPanel);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.pack();
 		myFrame.setVisible(true);
@@ -48,63 +52,66 @@ public class Desktop2 {
 		Timer myTimer = new Timer(30, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// myPanel knows enough to do the job
-				obj2.update(); // send signal to JPanel
-				obj2.repaint();
+				myPanel.update(); // send signal to JPanel
+				myPanel.collision(obj);
+				myPanel.repaint();
 			}
 		});
 		myTimer.start();
+	}
+	
+	private static void generateThrowables() {
+		throwList.add(new Throwable(new StringMovingObject("Rock", 400, 100)));
+		throwList.add(new Throwable(new StringMovingObject("Scissor", 400, 100)));
 	}
 
 }
 
 // inner class, trying to be like applet, with all code in one file
 class DesktopPanel extends JPanel {
+	private MovingObject obj;
+	private ArrayList<Throwable> objList;
+
+	
+	public DesktopPanel(MovingObject obj) {
+		this.obj = obj;
+	}
+	
+	public DesktopPanel(ArrayList<Throwable> objList) {
+		this.objList = objList;
+	}
+	
+	
 	public Dimension getPreferredSize() {
 		return new Dimension(500, 500);
 	}
 
 	protected void update() {
-		throw1X--;
-		
-		throw2Y--;
+		//obj.update();
+		for(MovingObject obj : objList) {
+			obj.update();
+		}
+	}
+	
+	protected boolean collision(MovingObject obj) {
+		for(int i = 0; i < objList.size(); i++) {
+			for(int j = i + 1; j < objList.size(); j++) {
+				if(objList.get(i).collision(objList.get(j))) {
+					System.out.println("HERE");
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	protected void paintComponent(Graphics g) {
 		// clear previous screen
 		super.paintComponent(g);
-		// need Graphics2D to get font render context
 		Graphics2D g2d = (Graphics2D) g;
-		// usual font idioms
-		Font throwFont = new Font("Courier", Font.BOLD, 20);
-		FontRenderContext throwContext = g2d.getFontRenderContext();
-		// individual objects; in general, should be in an aggregate
-		throw1Rectangle = throwFont.getStringBounds(throw1Name, throwContext);
-		throw2Rectangle = throwFont.getStringBounds(throw2Name, throwContext);
-		// handle wraparound for throw1
-		if (throw1X + throw1Rectangle.getWidth() < 0)
-			throw1X = getWidth(); // this is getWidth() from Component
-		throw1Y = (int) -throw1Rectangle.getY();
-		g2d.drawString(throw1Name, throw1X, throw1Y);
-		// handle wraparound for throw2
-		if (throw2Y + throw2Rectangle.getHeight() < 0)
-			throw2Y = getHeight(); // this is getWidth() from Component
-		throw2X = (int) -throw2Rectangle.getX();
-		g2d.drawString(throw2Name, throw2X, throw2Y);
-		// test for collision
-		if (Math.abs(throw1X - throw2X) < 25 && Math.abs(throw1Y - throw2Y) < 25) {
-			g2d.setColor(Color.RED);
-			g2d.drawString(bang, 200, 200);
+		for(MovingObject obj : objList) {
+			obj.paintComponent(g);
 		}
+		//obj.paintComponent(g);
 	}
-
-	private String bang = "BANG!";
-	// these should all be created by a Builder
-	private String throw1Name = "Rock";
-	private static Rectangle2D throw1Rectangle;
-	private static int throw1X = 500;
-	private static int throw1Y;
-	private String throw2Name = "Paper";
-	private static Rectangle2D throw2Rectangle;
-	private static int throw2X;
-	private static int throw2Y = 500;
 }
