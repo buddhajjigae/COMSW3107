76991c19286fea896f8d9ecb6d196fd7b9716e6d
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-12-10 03:53:09 -0500

    Update Throwable.java

--- RPLSKGUI/Throwable.java
+++ RPLSKGUI/Throwable.java
@@ -1,5 +1,6 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.geom.Rectangle2D;
 
 public class Throwable implements MovingObject {
 	MovingObject object;
@@ -8,15 +9,48 @@ public class Throwable implements MovingObject {
 		this.object = obj;
 	}
 	
-	public void paintComponent(Graphics g) {
-		object.paintComponent(g);
-	}
-
-	public Point getLocation() {
-		return null;
-	}
-	
 	public void update() {
 		object.update();
 	}
+
+	public void paintComponent(Graphics g) {	
+		object.paintComponent(g);
+	}
+
+	public boolean collision(MovingObject obj) {
+		double height = obj.getHeight();
+		double width = obj.getWidth();
+		double x = obj.getX() - .5 * height;
+		double y = obj.getY() - .5 * width;
+		Rectangle2D rect = object.getRectangle();
+		if(rect.intersects(x, y, height, width)) {
+			System.out.println("====================");
+			System.out.println("WORKED");
+			System.out.println("====================");
+			return true;
+		}
+		return false;
+	}
+
+	public int getX() {
+		return object.getX();
+	}
+
+	public int getY() {
+		return object.getY();
+	}
+	
+	public double getHeight() {
+		return object.getHeight();
+	}
+	
+	public double getWidth() {
+		return object.getWidth();
+	}
+	
+	public Rectangle2D getRectangle() {
+		return object.getRectangle();
+	}
+
+
 }
