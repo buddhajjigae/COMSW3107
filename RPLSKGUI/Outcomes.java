965d3e59ec0e65daa5fe83f2a69c3f11fedfa092
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-12-13 23:50:10 -0500

    Update Outcomes.java
    Added strings and getter for string

--- RPLSKGUI/Outcomes.java
+++ RPLSKGUI/Outcomes.java
@@ -1,4 +1,13 @@
-
 public enum Outcomes {
-	WIN, LOSS, TIE
+	WIN("BEATS"), LOSS("LOSES TO"), TIE("TIES WITH");
+	
+	private final String outcome;
+	
+	Outcomes(String outcome) {
+		this.outcome = outcome;
+	}
+	
+	public String getOutcome() {
+		return outcome;
+	}
 }
