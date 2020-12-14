21d87bcb54b6419a8b12f67a40725c53a6bd1845
Alex Yu <72051877+buddhajjigae@users.noreply.github.com>
2020-12-13 23:49:30 -0500

    Update MovingObject.java
    Removed imports and changed functions

--- RPLSKGUI/MovingObject.java
+++ RPLSKGUI/MovingObject.java
@@ -1,13 +1,13 @@
-import java.awt.Graphics;
-import java.awt.Point;
+import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
 
 public interface MovingObject {
 	void update();
-	void paintComponent(Graphics g);
-	int getX();
-	int getY();
+	void paintComponent(Graphics2D g2d);
+	double getX();
+	double getY();
 	double getHeight();
 	double getWidth();
 	Rectangle2D getRectangle();
+	ThrowTypes getName();
 }
